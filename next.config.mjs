c7de7f9d5064735aaf8ef83f414f9df286c74737
Muaz Abdulkhafur <muwi1882@gmail.com>
2025-09-09 20:41:04 +0300

    update

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,19 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload'
+          }
+        ]
+      }
+    ]
+  }
 }
 
 export default nextConfig
